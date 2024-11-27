2c52cf60f547981b9faa52bd18dd14ef06471797
Zeta-05 <vjimmy276@gmail.com>
2024-11-27 09:02:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", "@ZTX_HEAD_QUATERS"))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "@ZTX_HEADQUATERS"))
+LOGGER_ID = int(getenv("LOGGER_ID", ""))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", "7678359785"))
